008e7dea76bf6c08dd89eb20e290f98f84054d5c
Alan Liang <symb@olic.link>
2024-08-29 15:31:41 +0800

    refactor(al): add CaptureI, remove remove_or

--- spectec/src/backend-prose/print.ml
+++ spectec/src/backend-prose/print.ml
@@ -307,6 +307,9 @@ let rec string_of_instr' depth instr =
   | PopAllI e ->
     sprintf "%s Pop all values %s from the top of the stack." (make_index depth)
       (string_of_expr e)
+  | CaptureI e ->
+    sprintf "%s Let %s be the remaining instruction sequence." (make_index depth)
+      (string_of_expr e)
   | LetI (e1, e2) ->
     sprintf "%s Let %s be %s." (make_index depth) (string_of_expr e1)
       (string_of_expr e2)
