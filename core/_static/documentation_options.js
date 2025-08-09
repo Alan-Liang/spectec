30e68b827f1bdb153ab61ecf6978b127a20cae2b
Alan-Liang <Alan-Liang@users.noreply.github.com>
2025-08-09 07:52:06 +0000

    deploy: 7cac282b35d31b1109499ba845d906101ebd7d6d

--- core/_static/documentation_options.js
+++ core/_static/documentation_options.js
@@ -1,6 +1,5 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.0 (Draft 2024-09-24)',
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '3.0 (Draft 2025-08-09)',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
